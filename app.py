e4e3a448172d1bfa8e8d6fb41ad84c574183ef34
AlvinLandedSpecialist <alvin920405.o11@gmail.com>
2025-04-25 17:18:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,26 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from flask_migrate import Migrate
 from models import db
 from auth import auth_bp
-from config import Config
+from projects import project_bp  # 导入项目蓝图
 import os
 
 app = Flask(__name__)
-app.config.from_object(Config)
-
-@app.route('/')
-def home():
-    return "Welcome to the Home Page"
+app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'  # 确保设置一个安全的密钥
 
 # CORS 配置
 CORS(app)
 
 # 数据库配置
 db.init_app(app)
-migrate = Migrate(app, db)
 
 # JWT 配置
 jwt = JWTManager(app)
 
 # 注册蓝图
 app.register_blueprint(auth_bp)
+app.register_blueprint(project_bp)  # 注册项目蓝图
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
