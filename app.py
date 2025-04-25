cbc902e42d7b7affe179606936e3332bbddc4eb1
AlvinLandedSpecialist <alvin920405.o11@gmail.com>
2025-04-25 16:08:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
+# app.py
 from flask import Flask
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from models import db
 from auth import auth_bp
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
 CORS(app)
+db.init_app(app)
+jwt = JWTManager(app)
 
 app.register_blueprint(auth_bp)
 
+# 初始化数据库
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
