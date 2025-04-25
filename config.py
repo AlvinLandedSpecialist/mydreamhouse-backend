abb70649d28561a8b1a5c655676500140a8bba26
AlvinLandedSpecialist <alvin920405.o11@gmail.com>
2025-04-25 16:14:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 class Config:
-    JWT_SECRET_KEY = "your-super-secret-key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///mydreamhouse.db"  # 可换为您的数据库地址
+    JWT_SECRET_KEY = "your-secret-key"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///mydreamhouse.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
