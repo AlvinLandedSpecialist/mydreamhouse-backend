92dbf81633cf2006dc2cb0238bca7f49dd463467
AlvinLandedSpecialist <alvin920405.o11@gmail.com>
2025-04-25 19:01:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -103,5 +103,5 @@ def update_project(id):
 
 # --- 启动程序 ---
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
