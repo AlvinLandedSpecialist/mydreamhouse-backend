26834cb758c3149e18df63df74897ac0e524d1a7
AlvinLandedSpecialist <alvin920405.o11@gmail.com>
2025-04-25 16:49:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,6 @@ jwt = JWTManager(app)
 # 注册蓝图
 app.register_blueprint(auth_bp)
 
-@app.before_first_request
-def create_tables():
-    # 使用数据库迁移替代此处创建表的方式
-    db.create_all()
-
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
