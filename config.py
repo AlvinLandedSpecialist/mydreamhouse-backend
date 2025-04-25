8717d8d7d0c309b980152c2819ba08436855ea7c
AlvinLandedSpecialist <alvin920405.o11@gmail.com>
2025-04-25 17:47:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 class Config:
-    JWT_SECRET_KEY = "your-secret-key"
+    JWT_SECRET_KEY = "blackforest"
     SQLALCHEMY_DATABASE_URI = "sqlite:///mydreamhouse.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
